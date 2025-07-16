0be596c2092afd1cbc543312cf6959aca4cd781e
minagishl <mvxd568.co@icloud.com>
2025-07-16 18:02:29 +0900

    feat: update browser handling

--- src/main.ts
+++ src/main.ts
@@ -51,10 +51,9 @@ async function main(): Promise<void> {
 		discord = new DiscordWebhook(env.discord_webhook_url);
 	}
 
-	const args = ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'];
+	const args = ['--no-sandbox', '--disable-setuid-sandbox'];
 
 	const browser = await puppeteer.launch({
-		headless: true,
 		defaultViewport: { width: 1080, height: 1024 },
 		args,
 	});
